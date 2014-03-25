2b54c83365745d21968d3fb4bf243bdb0d1bea1f
zizih <zizihjk@gmail.com>
2014-03-25 10:57:23 +0800

    rm @Documented

--- src/interactivity/dpa/db/Colum.java
+++ src/interactivity/dpa/db/Colum.java
@@ -12,7 +12,6 @@ import java.lang.annotation.*;
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@Inherited
 public @interface Colum {
 
     public String name();
--- src/interactivity/dpa/db/Table.java
+++ src/interactivity/dpa/db/Table.java
@@ -12,7 +12,6 @@ import java.lang.annotation.*;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@Inherited
 public @interface Table {
 
     public String name();
